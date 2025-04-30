54d1eb8856fac51a1a5cde81983f8e6e002c3c06
parsapaktinat <parsapaktinat63@gmail.com>
2025-04-30 21:51:15 +0330

    Add customer almost added do it later

--- sharifOOPBaby403/HW5_Q3_403101518/main.cpp
+++ sharifOOPBaby403/HW5_Q3_403101518/main.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define cs const string &
+
 class BankAccount {
 private:
     static double annualInterestRate;
@@ -13,11 +15,36 @@ private:
     static void applyTaxToAll() {
 
     }
+
+    string name;
+    string nationalID;
+    string accountNumber;
+    string initialBalance;
+public:
+    BankAccount(cs name,cs nationalID, cs accountNumber, cs initialBalance) : name(name), nationalID(nationalID),
+    accountNumber(accountNumber), initialBalance(initialBalance) {}
+
+    string getName() const {return name;}
+    void setName(cs name) {this->name = name;}
+    string getNatioalID() const {return nationalID;}
+    void setNationalID(cs name) {this->nationalID = nationalID;}
+    string getName() const {return name;}
+    void setName(cs name) {this->name = name;}
+    string getName() const {return name;}
+    void setName(cs name) {this->name = name;}
+
 };
 
 class Customer {
 private:
     static vector<BankAccount> customerList;
+public:
+    int addCustomer(cs name, cs nationalID, cs accountNumber, cs initialBalance) {
+        for (auto customer : customerList) {
+            if (customer.)
+        }
+
+    }
 };
 
 class View {
@@ -36,7 +63,14 @@ public:
                 cp.push_back(word);
 
             // Add a new customer
+            if (cp[0] == "add_customer" && cp.size() == 5) {
+                string name = cp[1];
+                string nationalID = cp[2];
+                string accountNumber = cp[3];
+                string initialBalance = cp[4];
 
+
+            }
         }
     }
 };
