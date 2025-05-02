d3baf6a0af93fd908f12c1d882a83d72daea99be
parsapaktinat <parsapaktinat63@gmail.com>
2025-05-02 13:34:07 +0330

    The configuration changed. Add Account and Deposit added.

--- sharifOOPBaby403/HW5_Q3_403101518/main.cpp
+++ sharifOOPBaby403/HW5_Q3_403101518/main.cpp
@@ -3,8 +3,33 @@ using namespace std;
 
 #define cs const string &
 
-class BankAccount {
+class BankAccount;
+
+class Customer {
 private:
+    string name;
+    string nationalID;
+    string accountNumber;
+    double initialBalance = 0;
+
+public:
+    Customer(string name, string nationalID, string accountNumber, double initialBalance) : name(name), nationalID(nationalID), accountNumber(accountNumber) {};
+
+    static vector<Customer> customerList;
+
+    string getName() const {return name;}
+    void setName(cs name) {this->name = name;}
+    string getNationalID() const {return nationalID;}
+    void setNationalID(cs nationalID) {this->nationalID = nationalID;}
+    string getAccountNumber() const {return accountNumber;}
+    void setAccountNumber(cs accountNumber) {this->accountNumber = accountNumber;}
+    double getInitialBalance() const {return initialBalance;}
+    void setInitialBalance(double initialBalance) {this->initialBalance = initialBalance;}
+
+};
+
+class BankAccount {
+public:
     static double annualInterestRate;
     static double taxRate;
 
@@ -15,34 +40,29 @@ private:
     static void applyTaxToAll() {
 
     }
-
-    string name;
-    string nationalID;
-    string accountNumber;
-    string initialBalance;
-public:
-    BankAccount(cs name,cs nationalID, cs accountNumber, cs initialBalance) : name(name), nationalID(nationalID),
-    accountNumber(accountNumber), initialBalance(initialBalance) {}
-
-    string getName() const {return name;}
-    void setName(cs name) {this->name = name;}
-    string getNatioalID() const {return nationalID;}
-    void setNationalID(cs name) {this->nationalID = nationalID;}
-    string getName() const {return name;}
-    void setName(cs name) {this->name = name;}
-    string getName() const {return name;}
-    void setName(cs name) {this->name = name;}
-
 };
 
-class Customer {
+class Control {
 private:
-    static vector<BankAccount> customerList;
+    Customer customer;
 public:
-    int addCustomer(cs name, cs nationalID, cs accountNumber, cs initialBalance) {
-        for (auto customer : customerList) {
-            if (customer.)
+    // Add new customer
+    int addCustomer(cs name, cs nationalID, cs accountNumber, double initialBalance) {
+        for (auto customer : Customer::customerList) {
+            if (customer.getName() == name)
+                return 1;
+            if (customer.getNationalID() == nationalID)
+                return 2;
+            if (customer.getAccountNumber() == accountNumber)
+                return 3;
         }
+        Customer newCustomer(name,nationalID,accountNumber,initialBalance);
+        Customer::customerList.push_back(newCustomer);
+        return 4;
+    }
+
+    // Deposit money
+    int depositMoney(cs accountNumebr, double money) {
 
     }
 };
@@ -68,9 +88,30 @@ public:
                 string nationalID = cp[2];
                 string accountNumber = cp[3];
                 string initialBalance = cp[4];
-
-
+                int status = customer.addCustomer(name, nationalID, accountNumber, initialBalance);
+                switch (status) {
+                    case 1:
+                        cout << "Error! this name is already registered." << endl;
+                        break;
+                    case 2:
+                        cout << "Error! this national_ID is already registered." << endl;
+                        break;
+                    case 3:
+                        cout << "Error! this accountNumber is already registered." << endl;
+                        break;
+                    case 4:
+                        cout << "Customer " << name << " added. Account: " << accountNumber << endl;
+                        break;
+                }
             }
+
+            // Deposit money
+            if (cp[0] == "deposit" && cp.size() == 3) {
+                string accountNumber = cp[1];
+                double amount = stod(cp[2]);
+                int status
+            }
+
         }
     }
 };
