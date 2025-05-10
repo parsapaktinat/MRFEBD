f1ba0dc9dc472d3cd0172f9f073b5a6241a74a68
parsapaktinat <parsapaktinat63@gmail.com>
2025-05-10 18:51:56 +0330

    classes added.

---
+++ sharifOOPBaby403/HW6_Q1_403101518/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Food {
+private:
+    int basePrice;
+};
+
+class Drink:public Food {
+
+};
+
+class Dessert:public Food {
+
+};
+
+class Main:public Food {
+
+};
+
+int main()
+{
+
+    return 0;
+}
