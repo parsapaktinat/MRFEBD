bb195e7a49f7836e8b41ad2a3daf0c569a365868
parsa <parsapaktinat63@gmail.com>
2025-05-18 03:25:05 +0330

    classes added

--- sharifOOPBaby403/HW6_Q2_403101518/main.cpp
+++ sharifOOPBaby403/HW6_Q2_403101518/main.cpp
@@ -1,7 +1,84 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Model
+class Node{
+private:
+    int Number;
+
+public:
+    double getVoltage() const {
+
+    }
+};
+
+class Component{
+protected:
+    string name;
+    Node Node1;
+    Node Node2;
+
+public:
+    virtual double getVoltage() const = 0;
+
+    virtual double getCurrent() const = 0;
+};
+
+class Resistor:public Component{
+public:
+    double getVoltage() const {
+
+    }
+
+    double getCurrent() const {
+
+    }
+};
+
+class VoltageSource:public Component{
+public:
+    double getVoltage() const {
+
+    }
+
+    double getCurrent() const {
+
+    }
+};
+
+// Control
+class ProcessCommand{
+private:
+    vector<Component *> components;
+
+public:
+    
+};
+
+// View
+class View{
+public:
+    void run() {
+        regex addNodePattern(R"(add\s+node (\S+)\s*)");
+
+        smatch matches;
+        string cmd;
+
+        while (true) {
+            getline(cin, cmd);
+
+            if(regex_match(cmd,matches,addNodePattern)){
+                string nodeName=matches[1];
+
+            }
+
+
+        } 
+    }
+};
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    
     return 0;
 }
