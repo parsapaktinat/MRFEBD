0751117e3417d5f9aec59a1b8cfcdcd1ad2800d7
parsa <parsapaktinat63@gmail.com>
2025-05-18 04:10:31 +0330

    'Add node' added

--- sharifOOPBaby403/HW6_Q2_403101518/main.cpp
+++ sharifOOPBaby403/HW6_Q2_403101518/main.cpp
@@ -1,16 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define cs const string &
+
 // Model
 class Node{
 private:
     int Number;
+    double voltage;
+    static int counter;
 
 public:
+    Node() {
+        Number = ++counter;
+        voltage = 0;
+    }
+
     double getVoltage() const {
 
     }
 };
+
+int Node::counter = 0;
 
 class Component{
 protected:
@@ -47,19 +58,39 @@ public:
 };
 
 // Control
-class ProcessCommand{
+class Circuit{
 private:
     vector<Component *> components;
+    unordered_map<string, Node> nodes;
 
 public:
-    
+    // Add node
+    void addNode(cs name) {
+        Node newNode;
+        nodes.emplace(newNode);
+    }
+
+    // Add resistor
+    void addResistor(cs resVal, cs resName, cs node1Name, cs node2Name) {
+        
+    }
+
+    // Destructor
+    ~Circuit() {
+        for (auto component:components) {
+            delete component;
+        }
+    }
 };
 
 // View
 class View{
+private:
+    Circuit spice;
 public:
     void run() {
-        regex addNodePattern(R"(add\s+node (\S+)\s*)");
+        regex addNodePattern(R"(add node (\S+)\s*)");
+        regex addResistorPattern(R"(add resistor (\S+)\s*)");
 
         smatch matches;
         string cmd;
@@ -67,18 +98,30 @@ public:
         while (true) {
             getline(cin, cmd);
 
-            if(regex_match(cmd,matches,addNodePattern)){
-                string nodeName=matches[1];
+            // Add node
+            if (regex_match(cmd, matches, addNodePattern)) {
+                string nodeName = matches[1];
+                spice.addNode(nodeName);
+            }
+
+            // Add resistor
+            else if (regex_match(cmd, matches, addResistorPattern)) {
+                string resVal = matches[1];
+                string resName = matches[2];
+                string node1Name = matches[3];
+                string node2Name = matches[4];
 
             }
 
 
+            // End
+            else if (cmd == "end")
+                return;
         } 
     }
 };
 
-int main()
-{
+int main() {
     
     return 0;
 }
