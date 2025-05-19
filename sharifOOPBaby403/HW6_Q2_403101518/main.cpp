be110f5beb8129c8c358a0bd916d9ae4cf024491
parsapaktinat <parsapaktinat63@gmail.com>
2025-05-19 17:41:05 +0330

    Add res and add voltage source added.

--- sharifOOPBaby403/HW6_Q2_403101518/main.cpp
+++ sharifOOPBaby403/HW6_Q2_403101518/main.cpp
@@ -16,9 +16,7 @@ public:
         voltage = 0;
     }
 
-    double getVoltage() const {
-
-    }
+    double getVoltage() const {return voltage;}
 };
 
 int Node::counter = 0;
@@ -30,27 +28,31 @@ protected:
     Node Node2;
 
 public:
+    Component(cs name, Node node1, Node node2) : name(name), Node1(node1), Node2(node2) {}
+
     virtual double getVoltage() const = 0;
 
     virtual double getCurrent() const = 0;
 };
 
 class Resistor:public Component{
+private:
+    double value;
 public:
-    double getVoltage() const {
+    Resistor(cs name, Node Node1, Node Node2,double value) : Component(name, Node1, Node2), value(value) {}
 
-    }
+    double getVoltage() const {}
 
-    double getCurrent() const {
-
-    }
+    double getCurrent() const {}
 };
 
 class VoltageSource:public Component{
+private:
+    double voltage;
 public:
-    double getVoltage() const {
+    VoltageSource(Node node1, Node node2 , double voltage) : Component("V1", node1, node2), voltage(voltage) {}
 
-    }
+    double getVoltage() const {return voltage;}
 
     double getCurrent() const {
 
@@ -66,13 +68,20 @@ private:
 public:
     // Add node
     void addNode(cs name) {
-        Node newNode();
-        nodes.emplace(newNode);
+        Node newNode;
+        nodes.emplace(name,newNode);
     }
 
     // Add resistor
-    void addResistor(cs resVal, cs resName, cs node1Name, cs node2Name) {
-        
+    void addResistor(double resVal, cs resName, cs node1Name, cs node2Name) {
+        Component* newResistor = new Resistor(resName, nodes.at(node1Name), nodes.at(node2Name),resVal);
+        components.emplace_back(newResistor);
+    }
+
+    // Add voltage source
+    void addVoltageSource(double voltage, cs node1, cs node2) {
+        Component* newVoltageSource = new VoltageSource(nodes.at(node1),nodes.at(node2),voltage);
+        components.emplace_back(newVoltageSource);
     }
 
     // Destructor
@@ -91,6 +100,7 @@ public:
     void run() {
         regex addNodePattern(R"(add node (\S+)\s*)");
         regex addResistorPattern(R"(add resistor (\S+)\s*)");
+        regex addVoltageSourcePattern(R"(add voltage source (\S+)\s*)");
 
         smatch matches;
         string cmd;
@@ -106,11 +116,19 @@ public:
 
             // Add resistor
             else if (regex_match(cmd, matches, addResistorPattern)) {
-                string resVal = matches[1];
+                double resVal = stod(matches[1]);
                 string resName = matches[2];
                 string node1Name = matches[3];
                 string node2Name = matches[4];
+                spice.addResistor(resVal, resName, node1Name, node2Name);
+            }
 
+            // Add voltage source
+            else if (regex_match(cmd, matches, addVoltageSourcePattern)) {
+                double voltageSource = stod(matches[1]);
+                string node1Name = matches[2];
+                string node2Name = matches[3];
+                spice.
             }
 
 
