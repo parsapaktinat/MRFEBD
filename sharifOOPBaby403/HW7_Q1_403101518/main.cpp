412bcefd8307864cf4c0d404fde2f784594ce28f
parsapaktinat <parsapaktinat63@gmail.com>
2025-05-21 00:44:52 +0330

    Add doctor already added but not yet (Javad Khiabani)

--- sharifOOPBaby403/HW7_Q1_403101518/main.cpp
+++ sharifOOPBaby403/HW7_Q1_403101518/main.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define cs const string &
+
 // ------------- Exception example ------------------
 class NameException : public exception {
 public:
@@ -10,27 +12,78 @@ public:
 };
 
 // ------------- your code goes here ----------------
+class Doctor {
+private:
+    string name;
+    string specialty;
+    int numberOfPatients;
+    vector<string> workingDays;
 
+public:
+    Doctor(cs _name, cs _specialty, int _numberOfPatients, const vector<string> &_workingDays) : name(_name), specialty(_specialty), numberOfPatients(_numberOfPatients), workingDays(_workingDays) {};
 
-bool inputHandler(string line) {
-    regex addDoctorPattern(R"(add doctor (\S+)\s*)");
+    string getName() const {return name;}
+    string getSpecialty() const {return specialty;}
+    int getNumberOfPatients() const {return numberOfPatients;}
+    vector<string> getWorkingDays() const {return workingDays;}
+};
 
-    smatch matches;
-    string cmd;
+class HospitalController {
+private:
+    vector<Doctor> doctors;
 
-    if (regex_match(cmd, matches, addDoctorPattern)) {
+public:
+    // Add doctor
+    int addDoctor(cs name, cs specialty, int numberOfPatients, vector<string> &workingDays) {
+        int i = -1;
+        if (findNameOfDoctor(i, name)) {
+
+        }
+    }
+
+    // Helper functions
+    bool findNameOfDoctor(int &i, cs name) {
+        for (int j = 0;j < doctors.size();j++) {
+            if (doctors[j].getName() == name) {
+                i = j;
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+bool inputHandler(string line, HospitalController &c) {
+    string word;
+    vector<string> cp;
+    stringstream ss(line);
+    while (ss >> word) {
+        cp.push_back(word);
+    }
+
+    // Add doctor
+    if (cp[0] == "add" && cp[1] == "doctor" && cp.size() >= 6) {
+        string name = cp[2];
+        string specialty = cp[3];
+        int numberOfPatients = stoi(cp[4]);
+        vector<string> workingDays;
+        for (int i = 5;i < cp.size();i++) {
+            workingDays.push_back(cp[i]);
+        }
 
     }
+
 }
 
 // ------ do not change main() function ------
 int main() {
+    HospitalController hc;
     string line;
     bool cond = true;
     while (cond) {
         getline(cin, line);
         try {
-            cond = inputHandler(line);
+            cond = inputHandler(line, hc);
         }
         catch (const exception& e) {
             cout << e.what() << endl;
