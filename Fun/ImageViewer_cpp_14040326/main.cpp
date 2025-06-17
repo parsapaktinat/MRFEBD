cb136b0669116623a68b4e8a04395c07f89315f1
parsapaktinat <parsapaktinat63@gmail.com>
2025-06-17 22:04:42 +0330

    The program was successful.

--- Fun/ImageViewer_cpp_14040326/main.cpp
+++ Fun/ImageViewer_cpp_14040326/main.cpp
@@ -1,59 +1,20 @@
-#include <SDL2/SDL.h>
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-        return 1;
+using namespace std;
+
+int main()
+{
+    cv::Mat image;
+    const string path = "/home/parsa/Downloads";
+    image = cv::imread(path, cv::IMREAD_COLOR);
+
+    if (image.empty()) {
+        cout << "Could not open or find the image" << endl;
+        return -1;
     }
 
-    // Create a window
-    SDL_Window* window = SDL_CreateWindow("SDL2 Graphing", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-    if (window == nullptr) {
-        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
-
-    // Create a renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == nullptr) {
-        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    // Main loop
-    bool quit = false;
-    SDL_Event e;
-    while (!quit) {
-        // Handle events
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            }
-        }
-
-        // Clear the screen
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-
-        // Draw graphics here
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderDrawLine(renderer, 100, 100, 700, 500);
-        SDL_Rect rect = { 200, 200, 100, 100 };
-        SDL_RenderFillRect(renderer, &rect);
-
-        // Update the screen
-        SDL_RenderPresent(renderer);
-    }
-
-    // Clean up
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
+    cv::imshow("Display window", image);
+    cv::waitKey(0);
     return 0;
 }
